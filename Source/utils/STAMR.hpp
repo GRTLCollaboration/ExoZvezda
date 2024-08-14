6918f93bfbb298b0e03dbaf43985d220c8407afb
Tamara <86721223+tamaraevst@users.noreply.github.com>
2024-08-14 10:34:34 +0100

    update the branch with split up examples, initial conditions and run clang

--- Source/utils/STAMR.hpp
+++ Source/utils/STAMR.hpp
@@ -9,6 +9,10 @@
 #include "GRAMR.hpp"
 #include "StarTracker.hpp"
 
+#ifdef USE_AHFINDER
+#include "AHFinder.hpp"
+#endif
+
 /// A child of Chombo's AMR class to interface with tools which require
 /// access to the whole AMR hierarchy, and those of GRAMR
 /**
@@ -19,12 +23,20 @@ class STAMR : public GRAMR
   public:
     StarTracker m_star_tracker;
 
+#ifdef USE_AHFINDER
+    AHFinder<> m_ah_finder;
+#endif
+
     STAMR() {}
 
     void set_interpolator(AMRInterpolator<Lagrange<4>> *a_interpolator) override
     {
         GRAMR::set_interpolator(a_interpolator);
         m_star_tracker.set_interpolator(a_interpolator);
+
+#ifdef USE_AHFINDER
+        m_ah_finder.set_interpolator(a_interpolator);
+#endif
     }
 };
 
