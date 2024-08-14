18b3d16b1618d55dd1394fda6f005988c854c561
Tamara <86721223+tamaraevst@users.noreply.github.com>
2024-08-14 10:34:34 +0100

    update the branch with split up examples, initial conditions and run clang

--- Source/InitialConditions/BosonStars/BosonStarParams.hpp
+++ Source/InitialConditions/BosonStars/BosonStarParams.hpp
@@ -11,26 +11,28 @@
 //! A structure for the input params for the boson star
 struct BosonStar_params_t
 {
-    int gridpoints; // numer of gridpoints used to create boson star
-    double central_amplitude_CSF; //!< Central amplitude of the star
+    int gridpoints;
+    double PSC;
+    double OMC;
+    bool BS_solver_verbosity;
+    double central_amplitude_CSF;
+    double scalar_mass;
+    double phi4_coeff;
+    bool solitonic;
+    double sigma_solitonic; // 0.0499;
     double phase;
-    int eigen; // radial eigenstate of the boson star (0=ground)
     double BS_separation;
     double mass;
     double BS_impact_parameter;
-    bool BS_binary;
-    bool BS_BH_binary;
-    double Newtons_constant;
-    double BlackHoleMass;
     double BS_rapidity;
     double mass_ratio;
     int n_power;
-    int id_choice; // initial data choice: 0 - plain superposition, 1 - Thomas' trick, 2 - fixing conformal factor method
     double radius_width1;
     double radius_width2;
     int conformal_factor_power;
     bool antiboson;
-    std::array<double, CH_SPACEDIM> star_centre; //!< coordinates of the centre of the star
+    std::array<double, CH_SPACEDIM>
+        star_centre; //!< coordinates of the centre of the star
 };
 
 #endif /* BOSONSTARPARAMS_HPP_ */
